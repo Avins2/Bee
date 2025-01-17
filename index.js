5f06a78240843158be71209fb89e59fa801d4151
Blackarab-tech <m15108486@gmail.com>
2025-01-17 12:47:28 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* this is the main file */
+
 
 const {
   default: dreadedConnect,
@@ -12,78 +14,45 @@ const {
 } = require("@whiskeysockets/baileys");
 const pino = require("pino");
 const { Boom } = require("@hapi/boom");
-
-const { readFileSync } = require('fs'); 
-const { join } = require('path'); 
-
-
 const fs = require("fs");
  const FileType = require("file-type");
 const { exec, spawn, execSync } = require("child_process");
 const axios = require("axios");
 const chalk = require("chalk");
 const figlet = require("figlet");
-
-const PhoneNumber = require("awesome-phonenumber");
-
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000;
-const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./Lib/Exif');
- const { isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('./Lib/Func');
+const port = process.env.PORT || 10000;
+const _ = require("lodash");
+const PhoneNumber = require("awesome-phonenumber");
+const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/exif');
+ const { isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('./lib/botFunctions');
 const store = makeInMemoryStore({ logger: pino().child({ level: "silent", stream: "store" }) });
-const { commands, totalCommands } = require('./Handler/commandHandler');
-const { getSettings } = require('./Mongodb/Settingsdb');
-const messageHandler = require("./Handler/messageHandler");
-const eventHandler = require("./Handler/eventHandler.js");
-const eventHandler2= require("./Handler/eventHandler2.js");
-const handleMessage = require("./Handler/messageHandler");
-const connectionHandler = require('./Handler/connectionHandler');
 
-const handleCall = require('./Handler/callHandler');
+const authenticationn = require('./auth.js');
+const { smsg } = require('./smsg');
 
-
-const { connectToDB } = require('./Mongodb/loadDb');
-const { smsg } = require('./Handler/smsg.js');
-const botname = process.env.BOTNAME || '🐝𝐁𝐔𝐌𝐁𝐋𝐄 𝐁𝐄𝐄🐝';
-
-
-const authenticationn = require('./Auth/auth.js');
+const { autoview, autoread, botname, autobio, mode, prefix, presence, autolike } = require('./settings');
 const { DateTime } = require('luxon');
-
-
-
+const { commands, totalCommands } = require('./commandHandler');
 authenticationn();
-const mongoose = require("mongoose");
+const groupEvents = require("./groupEvents.js");
+// const connectionEvents = require("./connectionEvents.js");
 
+async function startDreaded() {
 
-
- async function startDreaded() {
-      await connectToDB();
-
-const settingss = await getSettings();
-
-
-
-const { autobio, mode, prefix, presence, anticall } = settingss;
-
-
-
-
-
-
-        const {  saveCreds, state } = await useMultiFileAuthState(`Session`)
+        const {  saveCreds, state } = await useMultiFileAuthState(`session`)
             const client = dreadedConnect({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: true,
 version: [2, 3000, 1015901307],
-        browser: [`🐝𝐁𝐔𝐌𝐁𝐋𝐄 𝐁𝐄𝐄🐝`,'Safari','3.0'],
+        browser: [`DREADED`,'Safari','3.0'],
 fireInitQueries: false,
             shouldSyncHistoryMessage: true,
             downloadHistory: true,
             syncFullHistory: true,
             generateHighQualityLinkPreview: true,
-            markOnlineOnConnect: false,
+            markOnlineOnConnect: true,
             keepAliveIntervalMs: 30_000,
         auth: state,
         getMessage: async (key) => {
@@ -97,13 +66,15 @@ fireInitQueries: false,
         }
     })
 
-store.bind(client.ev);
 
-setInterval(() => { store.writeToFile("store.json"); }, 3000);
+  store.bind(client.ev);
+
+  
 
 
+        setInterval(() => { store.writeToFile("store.json"); }, 3000);
 
-if (settingss && settingss.autobio === true){ 
+if (autobio === 'true'){ 
             setInterval(() => { 
 
                                  const date = new Date() 
@@ -118,20 +89,80 @@ if (settingss && settingss.autobio === true){
 
 }
 
-const { handleCallAndBan } = require('./Mongodb/Userdb');  
+  client.ev.on("messages.upsert", async (chatUpdate) => {
+    
+    try {
+      mek = chatUpdate.messages[0];
+      if (!mek.message) return;
+      mek.message = Object.keys(mek.message)[0] === "ephemeralMessage" ? mek.message.ephemeralMessage.message : mek.message;
 
 
-client.ws.on('CB:call', async (json) => {
-  const settingsss = await getSettings(); 
-  await handleCall(client, json, settingsss, handleCallAndBan);
-});
 
 
-client.ev.on("messages.upsert", (chatUpdate) => {
-  handleMessage(client, chatUpdate, store);
-});
 
-  
+
+
+if (autoview === 'true' && autolike === 'true' && mek.key && mek.key.remoteJid === "status@broadcast") {
+
+const mokayas = await client.decodeJid(client.user.id);
+
+if (mek.status) return;
+
+await client.sendMessage(mek.key.remoteJid, { react: { key: mek.key, text: '💚'}}, { statusJidList: [mek.key.participant, mokayas] });
+}
+
+
+            if (autoview === 'true' && mek.key && mek.key.remoteJid === "status@broadcast") { 
+         await client.readMessages([mek.key]);}
+else if (autoread === 'true' && mek.key && mek.key.remoteJid.endsWith('@s.whatsapp.net')) { 
+
+await client.readMessages([mek.key]);
+
+}
+
+if (mek.key && mek.key.remoteJid.endsWith('@s.whatsapp.net')) { 
+
+
+const Chat = mek.key.remoteJid;
+if(presence === 'online')
+
+            {await client.sendPresenceUpdate("available",Chat);}
+            else if(presence === 'typing')
+            {await client.sendPresenceUpdate("composing",Chat);}
+            else if(presence === 'recording')
+            {
+            await client.sendPresenceUpdate("recording", Chat);
+            }
+            else
+            {
+                await client.sendPresenceUpdate("unavailable", Chat);
+            }
+}
+
+
+      if (!client.public && !mek.key.fromMe && chatUpdate.type === "notify") return;
+      
+      m = smsg(client, mek, store);
+      require("./dreaded")(client, m, chatUpdate, store);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  // Handle error
+  const unhandledRejections = new Map();
+  process.on("unhandledRejection", (reason, promise) => {
+    unhandledRejections.set(promise, reason);
+    console.log("Unhandled Rejection at:", promise, "reason:", reason);
+  });
+  process.on("rejectionHandled", (promise) => {
+    unhandledRejections.delete(promise);
+  });
+  process.on("Something went wrong", function (err) {
+    console.log("Caught exception: ", err);
+  });
+
+  // Setting
   client.decodeJid = (jid) => {
     if (!jid) return jid;
     if (/:\d+@/gi.test(jid)) {
@@ -140,15 +171,8 @@ client.ev.on("messages.upsert", (chatUpdate) => {
     } else return jid;
   };
 
-  client.ev.on("contacts.update", (update) => {
-    for (let contact of update) {
-      let id = client.decodeJid(contact.id);
-      if (store && store.contacts) store.contacts[id] = { id, name: contact.notify };
-    }
-  });
-
-
- client.getName = (jid, withoutContact = false) => {
+ 
+  client.getName = (jid, withoutContact = false) => {
     id = client.decodeJid(jid);
     withoutContact = client.withoutContact || withoutContact;
     let v;
@@ -171,28 +195,97 @@ client.ev.on("messages.upsert", (chatUpdate) => {
     return (withoutContact ? "" : v.name) || v.subject || v.verifiedName || PhoneNumber("+" + jid.replace("@s.whatsapp.net", "")).getNumber("international");
   };
 
-
+  
   client.public = true;
 
   client.serializeM = (m) => smsg(client, m, store);
 
-
   client.ev.on("group-participants.update", async (m) => {
-    eventHandler2(client, m);
-
-eventHandler(client, m);
+    groupEvents(client, m);
   });
 
 
+    client.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect } = update;
+    if (connection === "close") {
+      let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
+      if (reason === DisconnectReason.badSession) {
+        console.log(`Bad Session File, Please Delete Session and Scan Again`);
+        process.exit();
+      } else if (reason === DisconnectReason.connectionClosed) {
+        console.log("Connection closed, reconnecting....");
+        startDreaded();
+      } else if (reason === DisconnectReason.connectionLost) {
+        console.log("Connection Lost from Server, reconnecting...");
 
-client.ev.on("connection.update", async (update) => {
-  await connectionHandler(client, update, startDreaded);
-});
+        startDreaded();
+      } else if (reason === DisconnectReason.connectionReplaced) {
+        console.log("Connection Replaced, Another New Session Opened, Please Restart Bot");
+        process.exit();
+      } else if (reason === DisconnectReason.loggedOut) {
+        console.log(`Device Logged Out, Please Delete File creds.json and Scan Again.`);
+        process.exit();
+      } else if (reason === DisconnectReason.restartRequired) {
+        console.log("Restart Required, Restarting...");
+        startDreaded();
+      } else if (reason === DisconnectReason.timedOut) {
+        console.log("Connection TimedOut, Reconnecting...");
+        startDreaded();
+      } else {
+        console.log(`Unknown DisconnectReason: ${reason}|${connection}`);
+        startDreaded();
+      }
+    } else if (connection === "open") {
+
+                 await client.groupAcceptInvite("HPik6o5GenqDBCosvXW3oe");
 
 
-client.ev.on("creds.update", saveCreds);
+        console.log(`✅ Connection successful\nLoaded ${totalCommands} commands.\nBot is active.`);
 
 
+        const getGreeting = () => {
+            const currentHour = DateTime.now().setZone('Africa/Nairobi').hour;
+
+            if (currentHour >= 5 && currentHour < 12) {
+                return 'Good morning 🌄';
+            } else if (currentHour >= 12 && currentHour < 18) {
+                return 'Good afternoon ☀️';
+            } else if (currentHour >= 18 && currentHour < 22) {
+                return 'Good evening 🌆';
+            } else {
+                return 'Good night 😴';
+            }
+        };
+
+
+        const getCurrentTimeInNairobi = () => {
+            return DateTime.now().setZone('Africa/Nairobi').toLocaleString(DateTime.TIME_SIMPLE);
+        };
+
+        let message = `Holla, ${getGreeting()},\n\nYou are connected to 🐝𝐁𝐔𝐌𝐁𝐋𝐄 𝐁𝐄𝐄🐝 bot. 📡 \n\n`;
+
+        message += `👤 𝑩𝑶𝑻𝑵𝑨𝑴𝑬:- ${botname}\n`;
+message += `🔓 𝑴𝑶𝑫𝑬:- ${mode}\n`;
+message += `✍️ 𝑷𝑹𝑬𝑭𝑰𝑿:- ${prefix}\n`;
+
+message += `📝 𝑪𝑶𝑴𝑴𝑨𝑵𝑫𝑺:- ${totalCommands}\n`
+        message += '🕝 𝑻𝑰𝑴𝑬:- ' + getCurrentTimeInNairobi() + '\n';
+        message += '💡 𝑳𝑰𝑩𝑹𝑨𝑹𝒀:- Baileys\n\n';
+
+message += `▞▚▞▚▞▚▞▚▞▚▞▚▞`
+
+
+
+
+            await client.sendMessage(client.user.id, { text: message });
+        }
+
+
+
+
+  });
+
+  client.ev.on("creds.update", saveCreds);
 
 
   client.sendText = (jid, text, quoted = "", options) => client.sendMessage(jid, { text: text, ...options }, { quoted });
@@ -209,6 +302,9 @@ client.ev.on("creds.update", saveCreds);
          return buffer 
       }; 
 
+
+       
+
  client.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => { 
          let quoted = message.msg ? message.msg : message; 
          let mime = (message.msg || message).mimetype || ''; 
@@ -224,10 +320,14 @@ client.ev.on("creds.update", saveCreds);
          await fs.writeFileSync(trueFileName, buffer); 
          return trueFileName; 
      };
+
 }
 
 
-app.use(express.static('Public'));
+
+
+
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/index.html'); 
@@ -236,8 +336,15 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
 
+startDreaded();
 
 
-startDreaded().catch(console.error);
+module.exports = startDreaded;
 
- 
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(chalk.redBright(`Update ${__filename}`));
+  delete require.cache[file];
+  require(file);
+});
