113ab72f0fbafb31718681d7552839a978c66cd3
Blackarab-tech <m15108486@gmail.com>
2025-01-17 16:10:04 +0300

    Delete Cmds/Owner/script.js

--- Cmds/Owner/script.js
+++
@@ -1,9 +0,0 @@
-//ping.js
-
-module.exports = async (context) => {
-        const { client, m } = context;
-
-
-await client.sendMessage(m.chat, { image: { url: 'https://i.imgur.com/hMoe3Rr.jpeg' }, caption: '🐝𝐁𝐔𝐌𝐁𝐋𝐄 𝐁𝐄𝐄🐝 REPO\n\nhttps://github.com/Blackarab-tech/Bee\n\nFork and star ✨'}, {quoted: m});
-
-}
